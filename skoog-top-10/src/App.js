25d934923bd96909040e5437dd72b01f7fcd3925
CodeFeff <feffmaste@gmail.com>
2025-08-03 22:36:22 +0200

    Update some text on main page

--- skoog-top-10/src/App.js
+++ skoog-top-10/src/App.js
@@ -7,13 +7,13 @@ function App() {
   return (
     <div className="App">
       <h1>Skoog Top 10</h1>
-      <h2>Next to release their top 10 Spotify playlist is:</h2>
+      <h2>Up next to share their Top 10 Spotify playlist:</h2>
       <img src={elin}></img>
       <br />
-      <span>Time until next playlist reveal</span>
+      <span>Countdown to reveal</span>
       <CountdownToDate targetDate="2025-08-07T16:00:00" />
       <br />
-      <span>Until then, enjoy some German Schlager </span>
+      <span>In the meantime, enjoy some German Schlager 🎶 </span>
       <iframe data-testid="embed-iframe" src="https://open.spotify.com/embed/playlist/352uZJbMoJhiCiDLL7UhQh?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     </div >
   );
