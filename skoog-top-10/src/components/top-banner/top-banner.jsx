c953f18712d7ad21faefdacb9c2e3ac556ee2305
CodeFeff <feffmaste@gmail.com>
2025-08-04 21:58:15 +0200

    Top banner added

---
+++ skoog-top-10/src/components/top-banner/top-banner.jsx
@@ -0,0 +1,10 @@
+import topBannerImg from '../../assets/top-bar.jpeg'
+import './top-banner.css'
+
+const TopBanner = () => {
+    return <div className="top-banner">
+        <h1 className='top-banner-title'>We ❤️ Music</h1>
+    </div>
+}
+
+export default TopBanner;
---
+++ skoog-top-10/src/views/home/Home.jsx
@@ -0,0 +1,9 @@
+import TopBannerComponent from "../../components/top-banner/top-banner";
+
+const Home = () => {
+    return <div>
+        <TopBannerComponent />
+    </div>
+}
+
+export default Home;
