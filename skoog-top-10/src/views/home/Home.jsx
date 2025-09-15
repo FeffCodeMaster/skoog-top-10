b84c60daf024085a85d09abef82c5f5f076e010d
CodeFeff <feffmaste@gmail.com>
2025-09-15 22:51:36 +0200

    Pushing Fernandos lista

--- skoog-top-10/src/views/home/Home.jsx
+++ skoog-top-10/src/views/home/Home.jsx
@@ -3,6 +3,7 @@ import CountdownSection from "../../components/countdown-section/countdown-secti
 import LatestReleaseSection from "../../components/latest-release-section/latest-release-section";
 import ReleaseList from "../../components/release-list/release-list";
 import config from "../../assets/configuration.json";
+import SectionWrapper from "../../components/section-wrapper/section-wrapper";
 
 import "./home.css"
 
@@ -13,7 +14,23 @@ const Home = () => {
         <TopBannerComponent />
         <div className="home-release-section">
             <LatestReleaseSection />
-            <CountdownSection />
+            <div className="home-youtube-container">
+                <SectionWrapper>
+                    <div className="home-youtube-container-title">Songs missing on Spotify</div>
+                    <div>
+                        <span>#3</span>
+                        <iframe src="https://www.youtube.com/embed/z96e2aGBhbo?si=eNmfxKDBUkVB3ztr" allowfullscreen></iframe>
+                    </div>
+                    <div>
+                        <span>#4</span>
+                        <iframe src="https://www.youtube.com/embed/IBUd6umotmI?si=U6Iubr6E1YsZXYej" allowfullscreen></iframe>
+                    </div>
+                    <div>
+                        <span>#6</span>
+                        <iframe src="https://www.youtube.com/embed/6dKKTTlfG3Q?si=o8GJ1vOplYliFCSA" allowfullscreen></iframe>
+                    </div>
+                </SectionWrapper>
+            </div>
         </div>
         <div className="home-release-list">
             <ReleaseList catergory={"Top 10 Best Songs"} />
