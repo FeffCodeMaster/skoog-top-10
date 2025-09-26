9620f88adb7fce331de215c48344113953d5fecc
CodeFeff <feffmaste@gmail.com>
2025-09-26 19:39:20 +0200

    Configure playlist for Emma

--- skoog-top-10/src/views/home/Home.jsx
+++ skoog-top-10/src/views/home/Home.jsx
@@ -14,23 +14,7 @@ const Home = () => {
         <TopBannerComponent />
         <div className="home-release-section">
             <LatestReleaseSection />
-            <div className="home-youtube-container">
-                <SectionWrapper>
-                    <div className="home-youtube-container-title">Songs missing on Spotify</div>
-                    <div>
-                        <span>#3</span>
-                        <iframe src="https://www.youtube.com/embed/z96e2aGBhbo?si=eNmfxKDBUkVB3ztr" allowfullscreen></iframe>
-                    </div>
-                    <div>
-                        <span>#4</span>
-                        <iframe src="https://www.youtube.com/embed/IBUd6umotmI?si=U6Iubr6E1YsZXYej" allowfullscreen></iframe>
-                    </div>
-                    <div>
-                        <span>#6</span>
-                        <iframe src="https://www.youtube.com/embed/6dKKTTlfG3Q?si=o8GJ1vOplYliFCSA" allowfullscreen></iframe>
-                    </div>
-                </SectionWrapper>
-            </div>
+            <CountdownSection />
         </div>
         <div className="home-release-list">
             <ReleaseList catergory={"Top 10 Best Songs"} />
